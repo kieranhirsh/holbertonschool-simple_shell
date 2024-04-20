519f5eabe38f75417689d665947771dc125dcfa8
kieranhirsh <kieranhirsh@gmail.com>
2024-04-20 12:54:36 +0000

    2. getenv()

---
+++ excercises/_getenv.c
@@ -0,0 +1,33 @@
+#include <stddef.h>
+
+/**
+ * _getenv - retruns an environment variable
+ * @name: the desired environment variable
+ *
+ * Return: the environment variable
+ */
+char *_getenv(const char *name)
+{
+	extern char **environ;
+	int ii = 0;
+	int jj;
+
+	while (environ[ii] != NULL)
+	{
+		jj = 0;
+
+		while ((name[jj] == environ[ii][jj]) && (name[jj] != NULL))
+			jj += 1;
+
+		if (name[jj] == NULL)
+		{
+			return (environ[ii] + jj + 1);
+		}
+
+		ii += 1;
+	}
+
+	return (NULL);
+
+}
+
---
+++ excercises/main_getenv.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+char *_getenv(const char *name);
+
+/**
+ * main - call _getenv
+ *
+ * Return: always 0
+ */
+int main(int argc, char **argv)
+{
+	int ii;
+
+	for (ii = 1 ; ii < argc ; ii++)
+		printf("%s = %s\n\n", argv[ii], _getenv(argv[ii]));
+
+	return (0);
+
+}
+
