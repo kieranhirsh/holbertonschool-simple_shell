40b100e78e71dc05a724bd90f0150e0adfed321b
kieranhirsh <kieranhirsh@gmail.com>
2024-04-24 05:57:07 +0000

    exit function implemented

--- interactive_shell.c
+++ interactive_shell.c
@@ -13,22 +13,29 @@ void interactive_shell(char **env)
 
 	path = get_path(env);
 
-	do {
-		printf("$ ");
+	while (1)	/* this loops infinitely until user exits the shell */
+	{
+		printf("$ ");	/* print prompt symbol */
 
+		/* get the command from the user input */
 		free(prompt);
 		prompt = get_command();
 		prompt = strtrim(prompt);
 		args = get_args(prompt);
+
+		/* execute the command, checking for some built-in functions */
+		if (strcmp(prompt, "exit") == 0)	/* if exit called */
+		{	/* avoid memory leaks and exit the shell */
+			free(prompt);
+			free(args);
+			free_dlistchar(path);
+			exit(EXIT_SUCCESS);
+		}
 		execute_command(prompt, args, path, env);
 
 		free(args);
 
-	} while (strcmp(prompt, "exit") != 0);
-
-	free(prompt);
-	free_dlistchar(path);
-	exit(EXIT_SUCCESS);
+	} 
 
 }
 
