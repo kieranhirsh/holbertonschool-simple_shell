cae1c10cf09824baa5de490485b49b99366cbe3e
root <kieranhirsh@gmail.com>
2024-04-25 15:41:45 +0000

    noninteractive shell now handles exit command

--- noninteractive_shell.c
+++ noninteractive_shell.c
@@ -17,10 +17,25 @@ void noninteractive_shell(char **env)
 		free(prompt);
 		prompt = get_command();
 		prompt = strtrim(prompt);
-		if (strcmp(prompt, "") != 0)	/* if prompt is not empty */
+		if (strcmp(prompt, "") != 0)		/* if prompt is not empty */
 		{
 			args = get_args(prompt);
-			execute_command(prompt, args, path, env);
+
+			/* execute the command, checking for some built-in functions */
+			if (strcmp(prompt, "exit") == 0)	/* if exit called */
+			{	/* avoid memory leaks and exit the shell */
+				free(prompt);
+				free(args);
+				free(path);
+				exit(EXIT_SUCCESS);
+			}
+			else
+			{
+				execute_command(prompt, args, path, env);
+			}
+		}
+		else					/* if prompt is empty */
+		{					/* do nothing */
 		}
 
 		free(args);
